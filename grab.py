636ba9123637060c0b228764a162fbaf716379b3
James <jlove7714@gmail.com>
2019-08-04 18:12:52 -0400

    Successfully builds HTML files with data from page. It does not get images or original formatting.

---
+++ grab.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2
+
+import urllib2
+import re
+
+urlShell = "https://projecteuler.net/problem="
+
+for i in range(1,667):
+    url = (urlShell + str(i))
+    response = urllib2.urlopen(url)
+    text = str(response.read())
+    fileName = re.search ("<h2>"r".*""</h2>", text)
+    fn = fileName.group().strip("<h2>")
+    fn = fn.strip("</")
+    fn = fn.replace(" ", "_")
+    fn = "/home/james/Documents/practice/exercises/" + str(i) + "." + fn + ".html"
+    print(fn)
+    file = open(fn, "w")
+    file.write(text)
+    file.close
