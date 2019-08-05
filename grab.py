22038c3bdf8d54aa1215a40681d4686347884e90
orangeswim <orangeswimstudios@gmail.com>
2019-08-05 04:49:14 +0000

    downloads txt files and images

--- grab.py
+++ grab.py
@@ -4,18 +4,37 @@ from BeautifulSoup import BeautifulSoup
 import urllib2
 import re
 
+import os
+import errno
+
 urlShell = "https://projecteuler.net/problem="
+base = "https://projecteuler.net/"
 
 def getLinks(html_page):
     #html_page = urllib2.urlopen(url)
     soup = BeautifulSoup(html_page)
     links = []
 
-    for link in soup.findAll('a'):
+    for link in soup.findAll('a', attrs={'href': re.compile(r'\w+\.(txt|png|jpg|jpeg)')}):
         links.append(link.get('href'))
 
     return links
 
+def saveFile(url, base, directory):
+    response = urllib2.urlopen(base + url)
+    text = str(response.read())
+    filename = directory + url
+    if not os.path.exists(os.path.dirname(filename)):
+        try:
+            os.makedirs(os.path.dirname(filename))
+        except OSError as exc: # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
+    with open(filename, "w") as f:
+        file.write(text)
+
+    return
 
 
 for i in [96]:
@@ -31,5 +50,8 @@ for i in [96]:
     file = open(fn, "w")
     file.write(text)
     file.close
-    print( getLinks(text) )
+    links =  getLinks(text) 
+    print links
+    for link in links:
+        saveFile(link, base, "exercises/")
 
